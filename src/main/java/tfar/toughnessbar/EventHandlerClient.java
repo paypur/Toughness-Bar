c04a16c37b82b45d44b28d78947b2ab5379d17cf
paypur <pieceofpaypur@gmail.com>
2025-03-07 15:07:11 -0500

    Port to 1.20.1
    Took 1 hour 12 minutes

--- src/main/java/tfar/toughnessbar/EventHandlerClient.java
+++ src/main/java/tfar/toughnessbar/EventHandlerClient.java
@@ -1,9 +1,8 @@
 package tfar.toughnessbar;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
@@ -26,8 +25,9 @@ public class EventHandlerClient {
 
   static IGuiOverlay ingameOverlay = EventHandlerClient::onRenderArmorToughnessEvent;
 
-  public static void onRenderArmorToughnessEvent(ForgeGui gui, PoseStack matrices, float partialTick, int width, int height) {
-    if (mc.getCameraEntity() instanceof LivingEntity viewEntity) {
+  public static void onRenderArmorToughnessEvent(ForgeGui gui, GuiGraphics graphics, float partialTick, int width, int height) {
+    assert mc.player != null;
+    if (mc.getCameraEntity() instanceof LivingEntity viewEntity && !mc.player.isCreative() && !mc.player.isSpectator()) {
       int armorToughness = Mth.floor(viewEntity.getAttribute(Attributes.ARMOR_TOUGHNESS).getValue());
       if (armorToughness < 1) {
         return;
@@ -44,39 +44,27 @@ public class EventHandlerClient {
       int color = getColor(layer);
       int previous = getColor(layer - 1);
 
-      RenderSystem.enableBlend();
-      //RenderSystem.pushMatrix();
-
       int top = height - gui.rightHeight;
       int right = width / 2 + 82;
-      bind(TEXTURE);
+
       for (int i = 0; i < 10; i++) {
         Index index = indexes[i];
         if (layer > 0)//toughness>20
           switch (index) {
-            case empty -> drawFullIcon(matrices, previous, i, right, top);
-            case half -> drawSplitIcon(matrices, previous, color, i, right, top);
-            case full -> drawFullIcon(matrices, color, i, right, top);
+            case empty -> drawFullIcon(graphics, previous, i, right, top);
+            case half -> drawSplitIcon(graphics, previous, color, i, right, top);
+            case full -> drawFullIcon(graphics, color, i, right, top);
           }
         else {//toughness<=20
           switch (index) {
-            case empty: if (ToughnessBar.ToughnessBarConfig.ClientConfig.empty.get()) drawEmptyIcon(matrices,color,i,right,top);break;
-            case half: drawHalfIcon(matrices,color,i,right,top);break;
-            case full: drawFullIcon(matrices,color,i,right,top);break;
+            case empty: if (ToughnessBar.ToughnessBarConfig.ClientConfig.empty.get()) drawEmptyIcon(graphics,color,i,right,top);break;
+            case half: drawHalfIcon(graphics,color,i,right,top);break;
+            case full: drawFullIcon(graphics,color,i,right,top);break;
           }
         }
       }
       gui.rightHeight += 10;
-
-      //Revert state
-      //RenderSystem.popMatrix();
-      bind(GuiComponent.GUI_ICONS_LOCATION);
-      RenderSystem.disableBlend();
     }
-  }
-
-  public static void bind(ResourceLocation tex) {
-    RenderSystem.setShaderTexture(0,tex);
   }
 
   public static void color(float r,float g,float b) {
@@ -105,37 +93,33 @@ public class EventHandlerClient {
     return index < 0 ? 0xffffff : index >= colors.size() ? colors.get(colors.size() - 1) : colors.get(index);
   }
 
-  private static void drawEmptyIcon(PoseStack stack,int color, int i, int guiLeft, int guiTop) {
+  private static void drawEmptyIcon(GuiGraphics graphics,int color, int i, int guiLeft, int guiTop) {
     color((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
-    blit(stack,guiLeft - i * 8, guiTop, 18,27, 9, 9);
+    blit(graphics,guiLeft - i * 8, guiTop, 18,27, 9, 9);
   }
 
-  private static void drawFullIcon(PoseStack stack,int color, int i, int guiLeft, int guiTop) {
+  private static void drawFullIcon(GuiGraphics graphics,int color, int i, int guiLeft, int guiTop) {
     color((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
-    blit(stack,guiLeft- i * 8, guiTop, 9, 18, 9, 9);
+    blit(graphics,guiLeft- i * 8, guiTop, 9, 18, 9, 9);
   }
 
-  private static void drawHalfIcon(PoseStack stack,int color, int i, int guiLeft, int guiTop) {
+  private static void drawHalfIcon(GuiGraphics graphics,int color, int i, int guiLeft, int guiTop) {
     color((color >> 16 & 0xff) / 256f, (color >> 8 & 0xff) / 256f, (color & 0xff) / 256f);
-    if (ToughnessBar.ToughnessBarConfig.ClientConfig.empty.get())drawEmptyIcon(stack,color,i,guiLeft,guiTop);
-    blit(stack,guiLeft - i * 8, guiTop, 0, 9, 9, 9);
+    if (ToughnessBar.ToughnessBarConfig.ClientConfig.empty.get())drawEmptyIcon(graphics,color,i,guiLeft,guiTop);
+    blit(graphics,guiLeft - i * 8, guiTop, 0, 9, 9, 9);
   }
 
-  private static void drawSplitIcon(PoseStack stack,int color1,int color2, int i, int guiLeft, int guiTop) {
-    drawFullIcon(stack,color1,i,guiLeft,guiTop);
+  private static void drawSplitIcon(GuiGraphics graphics,int color1,int color2, int i, int guiLeft, int guiTop) {
+    drawFullIcon(graphics,color1,i,guiLeft,guiTop);
     color((color2 >> 16 & 0xff) / 256f, (color2 >> 8 & 0xff) / 256f, (color2 & 0xff) / 256f);
-    blit(stack,guiLeft - i * 8, guiTop, 0, 9, 9, 9);
+    blit(graphics,guiLeft - i * 8, guiTop, 0, 9, 9, 9);
   }
 
-  public static void blit(PoseStack stack,int x, int y, float u, float v, int width, int height) {
-    blit(stack,x,y,0,u,v,width,height,9,27);
+  public static void blit(GuiGraphics graphics,int x, int y, float u, float v, int width, int height) {
+    graphics.blit(TEXTURE,x,y,0,u,v,width,height,27,9);
   }
 
-  public static void blit(PoseStack stack,int x, int y, int z, float u, float v, int width, int height, int textureX, int textureY) {
-    GuiComponent.blit(stack,x,y,z,u,v,width,height,textureY,textureX);
-  }
-
-    enum Index {
+  enum Index {
     half, full, empty
   }
 }
